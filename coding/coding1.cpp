d9995ae773e1af16a3c44d1386a3c6341c0f9d5c
LINIMIT <dbstjdgus97@naver.com>
2024-01-11 00:32:29 +0900

    Priority Queue
    우선순위 큐

--- coding/coding1.cpp
+++ coding/coding1.cpp
@@ -1,109 +1,107 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <queue>
 
-class Node
+
+
+template<typename T, typename Predicate = less<T>>
+class PriorityQueue
 {
-public:
-	Node(const char* data) : data(data) 
+private:
+	//0(logN) 높이에 의존적이기 때문
+	void push(const T& data)
 	{
+		_heap.push_back(data);
 
+		int now = static_cast<int>(_heap.size()) - 1;
+
+		while (now > 0)
+		{
+			//부모인덱스
+			int next = (now - 1) / 2; 
+
+			//부모 노드와 비교해 더 작으면 패배
+			if (_heap[now] < _heap[next])
+			{
+				break;
+			}
+
+			//데이터 교체
+			::swap(_heap[now],_heap[next]);
+			now = next;
+
+		}
+	}
+	//0(logN)
+	void pop()
+	{
+		_heap[0] = _heap.back();
+		_heap.pop_back();
+
+		int now = 0;
+
+		while (true)
+		{
+			int left = 2 * now + 1;
+			int right = 2 * now + 2;
+
+			//리프에 도달한 경우
+			if (left >= (int)_heap.size())
+			{
+				break;
+			}
+
+			int next = now;
+
+			//왼쪽값이 현재 값보다 크다면
+			if (_heap[next] < _heap[left])
+			{
+				next = left;
+			}
+
+			//오른쪽이 현재 값보다 크다면
+			if (right < _heap.size() && _heap[next] < _heap[right])
+			{
+				next = right;
+			}
+
+			//왼쪽/오른쪽 둘 다 현재 값보다 작으면 종료
+			if (next == now)
+			{
+				break;
+			}
+
+			::swap(_heap[now], _heap[next]);
+			now = next;
+			
+		}
+	}
+
+	T& top()
+	{
+		return _heap[0];
+	}
+	bool empty()
+	{
+		return _heap.empty();
 	}
 public:
-	const char* data;
-	vector<Node*> children;
+	vector<T> _heap;
+	Predicate _predicate;
 };
-
-Node* CreateTree()
-{
-	Node* root = new Node("R1 개발실");
-	{
-		Node* node = new Node("디자인팀");
-		root->children.push_back(node);
-		{
-			Node* leaf = new Node("전투");
-			root->children.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("경제");
-			root->children.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("스토리");
-			root->children.push_back(leaf);
-		}
-
-	}
-	{
-		Node* node = new Node("프로그래밍팀");
-		root->children.push_back(node);
-		{
-			Node* leaf = new Node("클라");
-			root->children.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("서버");
-			root->children.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("엔진");
-			root->children.push_back(leaf);
-		}
-
-	}
-	{
-		Node* node = new Node("아트");
-		root->children.push_back(node);
-		{
-			Node* leaf = new Node("배경");
-			root->children.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("캐릭터");
-			root->children.push_back(leaf);
-		}
-		
-
-	}
-	return root;
-}
-
-void PrintTree(Node* root, int depth = 0)
-{
-	for (int i = 0; i < depth; i++)
-	{
-		cout << "-";
-	}
-	cout << root->data << endl;
-	int size = root->children.size();
-
-	for (int i = 0; i < size; i++)
-	{
-		Node* node = root->children[i];
-		PrintTree(node, depth + 1);
-	}
-}
-
-int GetHeight(Node* root)
-{
-	int ret = 1;
-
-	int size = root->children.size();
-	for (int i = 0; i < size; i++)
-	{
-		Node* node = root->children[i];
-		int h = GetHeight(node) + 1;
-		if (ret < h)
-		{
-			ret = h;
-		}
-	}
-	return ret;
-}
 void main()
 {
-	Node* root = CreateTree();
-	PrintTree(root);
-	int h = GetHeight(root);
+	vector<int> v;
+	priority_queue<int> pq;
+
+	pq.push(10);
+	pq.push(40);
+	pq.push(30);
+	pq.push(50);
+	pq.push(20);
+
+	int value = pq.top();
+	pq.pop();
 
 }
