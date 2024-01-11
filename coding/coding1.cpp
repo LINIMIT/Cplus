6860427f430dc732cbbc6166af2e1d9e2baa2491
LINIMIT <dbstjdgus97@naver.com>
2024-01-12 01:57:17 +0900

    DFS
    DFS

--- coding/coding1.cpp
+++ coding/coding1.cpp
@@ -4,87 +4,104 @@ using namespace std;
 #include <queue>
 
 
-//인접 리스트 방식
-void CreateGraph_2()
+//DFS(Depth)
+//DFS == 재귀함수 == 스택
+//BFS
+
+struct Vertex
 {
-	struct Vertex
-	{
-		int data;
-	};
 
-	vector<Vertex>  v(6);
+};
 
-	//2차원 벡터
-	vector<vector<int>> adjacent;
-	adjacent.resize(6);
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
 
-	//adjacent[0].push_back(1);
-	//adjacent[0].push_back(3);
+//내가 방문한 목록
+vector<bool> visited;
+
+void CreateGraph()
+{
+	vertices.resize(6);
+
+	//인접 리스트
+	/*adjacent = vector<vector<int>>(6);
+
 	adjacent[0] = { 1,3 };
 	adjacent[1] = { 0,2,3 };
 	adjacent[3] = { 4 };
-	adjacent[5] = { 4 };
+	adjacent[5] = { 4 };*/
 
+	//인접 행렬
 
-	//0-3 연결확인
-	bool connected = false;
+	adjacent = vector<vector<int>>
+	{
+		{0, 1, 0, 1, 0, 0},
+		{1, 0, 1, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+	};
+}
 
-	int size = adjacent[0].size();
+void DFS(int here)
+{
+	//방문한 인덱스 확인
+	visited[here] = true;
+	cout << "Visited : " << here << endl;
+
+	//인접한 경로 체크(리스트 버전)
+
+	//인접 리스트 0(V+E)
+	//모든 정점마다 호출되기 때문에 v
+	//간선의 개수 E 더하기
+	/*const int size = adjacent[here].size();
+
 	for (int i = 0; i < size; i++)
 	{
-		int vertex = adjacent[0][i];
-		if (vertex == 3)
-		{
-			connected = true; // 간선연결 확인
+		int there = adjacent[here][i];
 
+		//이미 방문을 했는지 확인
+		if (visited[there] == false)
+		{
+			DFS(there);
 		}
+	}*/
+
+
+	//인접 행렬 0(V^2)
+	//1.정점마다 한 번씩 이 함수에 들어오기 때문
+	//2. 이 for문을 v만큼 실행하기 때문
+	for (int there = 0; there < 6; there++)
+	{
+		//길은 있는가
+		if (adjacent[here][there] == 0)
+		{
+			continue;
+		}
+
+		//아직 방문안한 곳에 한해서 방문
+		if (visited[there] == false)
+			DFS(there);
+		
 	}
 }
 
-
-//인접 행렬 방식
-void CreateGraph_3()
+void DFSAll()
 {
-	struct Vertex
+	visited = vector<bool>(6, false);
+
+
+	for (int i = 0; i < 6; i++)
 	{
-		int data;
-	};
-
-	//[X][0][X][0][X][X]
-	//[0][X][0][0][X][X]
-	//[X][X][X][X][X][X]
-	//[X][X][X][X][0][X]
-	//[X][X][X][X][X][X]
-	//[X][X][X][X][0][X]
-
-	//6개의 벡터를 만들고 그 벡터의 안에 X(false)를 넣어줌
-	//adjacent[from][to]
-	//메모리 소모 심하지만, 빠른 접근
-	//인접 리스트는 일일이 검색해야하지만 행렬 방식에서는 그냥 인덱스를 검색하면됨
-	vector<vector<bool>> adjacent(6, vector<bool>(6, false));
-
-	adjacent[0][1] = true;
-	adjacent[0][3] = true;
-	adjacent[1][2] = true;
-	adjacent[1][3] = true;
-	adjacent[3][4] = true;
-	adjacent[5][4] = true;
-
-	//0-3 연결확인
-	bool connected = adjacent[0][3];
-
-	vector<vector<int>> adjecent2 =
-	{
-		{-1,15,-1,35,-1,-1},
-		{15,-1,+5,10,-1,-1},
-		{-1,+5,-1,-1,-1,-1},
-		{35,10,-1,-1,+5,-1},
-		{-1,-1,-1,+5,-1,+5},
-		{-1,-1,-1,-1,+5,-1}
-	};
+		if (visited[i] == false)
+			DFS(i);
+	}
 }
 void main()
 {
+	CreateGraph();
 
-
+	visited = vector<bool>(6, false);
+	DFS(0);
 }
