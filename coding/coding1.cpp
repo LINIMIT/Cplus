1efe91e656cd3a24f882bb841c606221c1299807
LINIMIT <dbstjdgus97@naver.com>
2024-01-12 22:43:18 +0900

    BFS

--- coding/coding1.cpp
+++ coding/coding1.cpp
@@ -16,8 +16,12 @@ struct Vertex
 vector<Vertex> vertices;
 vector<vector<int>> adjacent;
 
-//내가 방문한 목록
+//내가 방문한 목록(DFS)
 vector<bool> visited;
+
+//발견한 목록
+vector<bool> discovered;
+
 
 void CreateGraph()
 {
@@ -83,7 +87,7 @@ void DFS(int here)
 		//아직 방문안한 곳에 한해서 방문
 		if (visited[there] == false)
 			DFS(there);
-		
+
 	}
 }
 
@@ -98,10 +102,86 @@ void DFSAll()
 			DFS(i);
 	}
 }
+
+
+void BFS(int here)
+{
+
+	//누구에 의해 발견되었는지?
+	vector<int> parent(6, -1);
+	//시작점에서 얼만큼 떨어져 있는지?
+	vector<int> dist(6, -1);
+
+
+	queue<int> q;
+	q.push(here);
+
+	//새로 찾을 지역이 있을 경우
+	discovered[here] = true;
+
+	parent[here] = here;
+	dist[here] = 0;
+	//한번 다 스캔을 끝낸 후 더 이상 새로운 발견 지점을 찾지 못한 경우
+	while (q.empty() == false)
+	{
+		here = q.front();
+		q.pop();
+
+		//방문 
+		cout << "Visited : " << here << endl;
+
+		for (int there = 0; there < 6; there++)
+		{
+			if (adjacent[here][there] == 0)
+				continue;
+			if (discovered[there])
+				continue;
+
+			q.push(there);
+			discovered[there] = true;
+
+			parent[there] = here;
+			dist[there] = dist[here] + 1;
+		}
+
+
+		/*int size = adjacent[here].size();
+
+		for (int i = 0; i < size; i++)
+		{
+			int there = adjacent[here][i];
+
+			//이미 발견된 지점일 경우 패스
+			if (discovered[there])
+				continue;
+
+			q.push(there);
+			discovered[there] = true;
+
+		}*/
+	}
+
+}
+
+void BFSAll()
+{
+	discovered = vector<bool>(6, false);
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (discovered[i] == false)
+			BFS(i);
+	}
+}
 void main()
 {
-	CreateGraph();
+	/*CreateGraph();
 
 	visited = vector<bool>(6, false);
-	DFS(0);
+	DFS(0);*/
+
+	CreateGraph();
+
+	BFSAll();
+
 }
