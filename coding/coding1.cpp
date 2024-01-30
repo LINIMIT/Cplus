46dfd6ef71988614e7be0cff091d5cd48729b455
LINIMIT <dbstjdgus97@naver.com>
2024-01-30 17:29:27 +0900

    MergeSort
    병합정렬

--- coding/coding1.cpp
+++ coding/coding1.cpp
@@ -45,7 +45,7 @@ void SelectionSort(vector<int>& v)
 void HeapSort(vector<int>& v)
 {
 
-	priority_queue<int,vector<int>,greater<int>> pq;
+	priority_queue<int, vector<int>, greater<int>> pq;
 
 	// 0(NlogN)
 	for (int num : v)
@@ -62,8 +62,65 @@ void HeapSort(vector<int>& v)
 
 }
 
+void MergeResult(vector<int>& v, int left, int mid, int right)
+{
+	int leftIdx = left;
+	int rightIdx = mid + 1;
+
+	vector<int> temp;
+
+	while (leftIdx <= mid && rightIdx <= right)
+	{
+		if (v[leftIdx] <= v[rightIdx])
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+		else
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+
+	//哭率捞 刚历 场车促搁
+	if (leftIdx > mid)
+	{
+		while (rightIdx <= right)
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+	else
+	{
+		while (leftIdx <= mid)
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+	}
+
+	for (int i = 0; i < temp.size(); i++)
+	{
+		v[left + i] = temp[i];
+	}
+}
+
+
+void MergeSort(vector<int>& v, int left, int right)
+{
+	if (left >= right)
+		return;
+	int mid = (left + right) / 2;
+
+	MergeSort(v, left, mid);
+	MergeSort(v, mid + 1, right);
+	MergeResult(v,left,mid,right);
+}
+
 int main()
 {
 	vector<int> v{ 1,5,3,4,2 };
-	HeapSort(v);
+	MergeSort(v, 0, v.size() - 1);
 }
